9809279c5cee520b49f669d912ac04407bcba028
heartbans <52736090+heartbans@users.noreply.github.com>
2019-07-29 11:26:59 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,13 +7,8 @@ export PATH
 ######################################################
 
 #net_speeder
-ETH=$(eval "ifconfig | grep 'eth0'| wc -l")
-if [ "$ETH"  ==  '1' ] ; then
-	nohup /usr/local/bin/net_speeder eth0 "ip" >/dev/null 2>&1 &
-fi
-if [ "$ETH"  ==  '0' ] ; then
-	nohup /usr/local/bin/net_speeder venet0 "ip" >/dev/null 2>&1 &
-fi
+nohup /usr/local/bin/net_speeder eth0 "ip" >/dev/null 2>&1 &
+#nohup /usr/local/bin/net_speeder venet0 "ip" >/dev/null 2>&1 &
 #net_speeder
 
 if [[ -z "${AppName}" ]]; then
